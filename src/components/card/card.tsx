8753b4d5fd1b839f02eb2e6b63c25ceb6bceefd3
Vitor Oliveira Ropke <vitorropke@hotmail.com>
2023-10-09 11:18:50 -0300

    Alguns ajustes no card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -11,14 +11,26 @@ interface CardProps {
 }
 
 export function Card({ brand, model, batch, quantity, price, discount, imageUrl }: CardProps) {
+    var color: string = '';
+    if (quantity < 100) {
+        color = 'yellow-text';
+    }
+    if (quantity < 30) {
+        color = 'red-text';
+    }
+
     return (
         <div className='card'>
-            <img src={imageUrl}></img>
-            <h1>{brand} {model}</h1>
-            <h3>Lote: {batch}</h3>
-            <h3>Preco: R${price}</h3>
-            <h3>Desconto: R${discount}</h3>
-            <h2>Quantidade: {quantity}</h2>
+            <div className='card-image'>
+                <img src={imageUrl}></img>
+            </div>
+            <div className='card-content'>
+                <h1>{brand} {model}</h1>
+                <h4>Lote: {batch}</h4>
+                <h4>Preco: R${price.toFixed(2)}</h4>
+                <h4>Desconto: R${discount.toFixed(2)}</h4>
+                <h2 className={color}>Quantidade: {quantity}</h2>
+            </div>
         </div>
     );
 }
